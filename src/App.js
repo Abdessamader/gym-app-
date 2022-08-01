76aa6705ed22cc13224ddd169ae0476fb7a3c15e
abdessamader <abdessamaderrifi46@gmail.com>
2022-08-01 11:34:04 +0700

    react-js-fitness-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (<>
+  
+  <h1 style={{textAlign:'center',marginTop:'14rem'}}>Hello</h1>
+ 
+
+
+
+  </>
+    
+  )
+}
+
+export default App
