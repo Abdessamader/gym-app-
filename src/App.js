d30f288aac61bb34e2e624e2bdc77f75f1d3e4fe
abdessamader <abdessamaderrifi46@gmail.com>
2022-08-15 11:16:12 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import { Box } from "@mui/material";
 import "./App.css";
 import ExerciceDetail from "./Pages/ExerciceDetail";
@@ -11,20 +11,17 @@ import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Router>
+    <Box width="400px" sx={{ width: {xl:'1488px' }}} m="auto">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exercices/:id" element={ExerciceDetail} />
+        <Route path="/" element={ <Home />} />
+        <Route path="/exercice/:id" element={ExerciceDetail} />
       </Routes>
-
-
-
-
-
 
       <Footer />
     </Box>
+    </Router>
   );
 };
 
