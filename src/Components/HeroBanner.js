122cb37996101066af87bd35d20b2397adcfb114
abdessamader <abdessamaderrifi46@gmail.com>
2022-08-18 10:18:54 +0700

    commit

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -4,7 +4,7 @@ import HeroBannerImage from '../assets/images/banner.png'
 const HeroBanner = () => {
   return (
     <Box sx={{
-        mt: { lg: '212px', xs:'70px'}, ml:{ sm:'50px' }
+        mt: { lg: '160px', xs:'70px'}, ml:{ sm:'50px' }
     }} position='relative' p='20px'>
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club    
